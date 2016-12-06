170d8f29b80f4bf1dcff5188b366cd5f7b9adb97
PK <karthikp@pageuppeople.com>
2016-12-07 09:07:05 +1100

    using the existing state file

--- dc0/plan.ps1
+++ dc0/plan.ps1
@@ -2,7 +2,7 @@ terraform get -update
 terraform remote config `
     -backend=s3 `
     -backend-config="bucket=terraform-dc0" `
-    -backend-config="key=base-vpc/tfstate/terraform.tfstate" `
+    -backend-config="key=base-vpc/tfstate/dc0/terraform.tfstate" `
     -backend-config="region=ap-southeast-2"
 terraform remote pull
 terraform plan ../
